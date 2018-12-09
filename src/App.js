8656090716a543472575bb3fb8680bc6d02d8f80
Angel Contreras <angelcbenoit@gmail.com>
2018-12-09 09:40:02 -0800

    use create-react-app v2.0 with scss and redux

--- src/App.js
+++ src/App.js
@@ -1,16 +1,15 @@
 import React, { Component } from 'react';
-import LandingPage from './components/LandingPage';
-import {BrowserRouter,Route} from 'react-router-dom';
-import { connect } from 'react-redux';
-//import * as actions from './actions';
+import { BrowserRouter, Route } from 'react-router-dom';
+import LandingPage from './LandingPage';
+import './scss/main.scss';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
+      <div className="items">
         <BrowserRouter>
           <div>
-              <Route exact path='/' component={LandingPage} />
+            <Route exact path='/' component={LandingPage} />
           </div>
         </BrowserRouter>
       </div>
--- src/index.js
+++ src/index.js
@@ -5,7 +5,7 @@ import { Provider } from 'react-redux';
 import { createStore, applyMiddleware } from 'redux';
 import reducers from './reducers/index';
 import reduxThunk from 'redux-thunk';
-import './css/style.css';
+import './scss/main.scss';
 
 const store = createStore(reducers, {}, applyMiddleware(reduxThunk));
 
@@ -14,3 +14,8 @@ ReactDOM.render(
         <App />
     </Provider>,
     document.getElementById('root'));
+
+// If you want your app to work offline and load faster, you can change
+// unregister() to register() below. Note this comes with some pitfalls.
+// Learn more about service workers: http://bit.ly/CRA-PWA
+//serviceWorker.unregister();
